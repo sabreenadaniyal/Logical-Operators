857b9140a1a705d005b70ef4132de7ce6a8716c6
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-03-16 00:56:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+//LOGICAL OPERATORS
+//AND(&&):
+let a = 10;
+let b = 5;
+console.log((a < b) && (a > b));
+//one more
+let c = 10;
+let d = 5;
+let e = 15;
+console.log((c < d) && (c > e));
+//OR(||):
+let f = 5;
+let g = 15;
+console.log((f > g) || (f < g));
+//NOT(!)
+let h = 20;
+let i = 25;
+console.log(!((h > i) && (h !== i)));
---
+++ main.ts
@@ -0,0 +1,22 @@
+//LOGICAL OPERATORS
+//AND(&&):
+let a:number=10
+let b:number=5
+console.log(
+(a < b) && (a > b) );   
+   //one more
+let c:number=10
+let d:number=5
+let e:number=15
+console.log(
+(c < d) && (c > e) );  
+//OR(||):
+let f:number=5
+let g:number=15
+console.log(
+(f > g) || (f < g) ); 
+//NOT(!)
+let h:number=20
+let i:number=25
+console.log(
+!((h > i) && (h!==i)) );     
